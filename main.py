ca3c194152f2e1113dccec1c64bbf54782547f6c
AparajitaFS <aparajitap@wilcosource.com>
2023-02-17 20:05:15 +0530

    Day 22: Render the titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -13,3 +14,19 @@ with col2:
               I am post graduated in 2018 with computer application
               """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python.
+"""
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
